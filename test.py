b629bf9bb84a7fedb32fcce5e047f9dacadd8798
dgl1797 <digiacomoluca1797@gmail.com>
2023-03-16 14:34:25 +0100

    altro dataset

---
+++ test.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+df1 = pd.read_csv('UpdatedResumeDataSet.csv')
+df2 = pd.read_csv('Resume.csv')
+
+categories1 = df['Category'].unique()
+categories2 = df['Category'].unique()
+
+print(category)
