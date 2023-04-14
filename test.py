10131ae9436b4740e13d0c6e0dba0bdb4774993b
dgl1797 <digiacomoluca1797@gmail.com>
2023-04-14 18:00:55 +0200

    primi passi

--- test.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df1 = pd.read_csv('UpdatedResumeDataSet.csv')
-df2 = pd.read_csv('Resume.csv')
-
-categories1 = df['Category'].unique()
-categories2 = df['Category'].unique()
-
-print(category)
